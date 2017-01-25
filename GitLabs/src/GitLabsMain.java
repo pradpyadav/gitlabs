bd00518c70ef78fb2273985a4edffe512aad70ad
Pradeep Yadav <pradpyadav@gmail.com>
2017-01-24 22:41:46 -0600

    2 Created a subrelease1.1 branch and updated GitLabsMain.

--- GitLabs/src/GitLabsMain.java
+++ GitLabs/src/GitLabsMain.java
@@ -1,10 +1,16 @@
 
 public class GitLabsMain {
 
+	int num1=10;
+	float fnum2=4.5f;
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
 		System.out.println("Welcome to the GitLabs");
+		GitLabsMain obj1=new GitLabsMain();
+		System.out.println("The value of the integer is >>> : "+obj1.num1);
+		System.out.println("The value of the float number is >>> : "+obj1.fnum2);
 	}
 
 }
